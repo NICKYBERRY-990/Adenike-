9b01ace28cf3c7bf5fe688a276ed5ea8cb42e8a0
NICKYBERRY-990 <onlineessentials990@gmail.com>
2024-11-26 12:58:52 +0000

    Update config.ts
    Meta title

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Adenike | Digital Marketing Specialist ';
+export const SITE_TITLE = 'Adenike Raheem | Digital Marketing Specialist ';
 export const SITE_DESCRIPTION = 'ADENIKE An Experience and efficient Office Assistant with a proven track record of excellence in the corporate environment..';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
